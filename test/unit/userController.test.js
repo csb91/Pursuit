7b8793123713e671c0f22cc9f5610ed344b9f1a4
carterbrooks <carter.brooks91@gmail.com>
2023-08-17 00:15:45 -0500

    add mocha/chai and test directory, try to start writing unit tests

---
+++ test/unit/userController.test.js
@@ -0,0 +1,9 @@
+const expect = require('chai').expect
+const assert = require('assert');
+const { addUser, getAllUsers } = require('../../prisma/controllers/users.js')
+
+describe('User', async () => {
+  it ('should get all the users', () => {
+
+  })
+})
